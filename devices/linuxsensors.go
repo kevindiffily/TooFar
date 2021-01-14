e851e306d19e24899a04d2fb0000d9eb770f23b0
Scot Bontrager <cloudkucooland@gmail.com>
2021-01-14 11:32:24 -0500

    make temp sensors saner

---
+++ devices/linuxsensors.go
@@ -0,0 +1,27 @@
+package devices
+
+import (
+	"github.com/brutella/hc/accessory"
+	// "github.com/brutella/hc/characteristic"
+	"github.com/brutella/hc/service"
+)
+
+// a system might have several chips
+type LinuxSensors struct {
+	*accessory.Accessory
+	Chips map[string]*SensorChipValues
+}
+
+// each chip might have several temps or fans
+type SensorChipValues map[string]*service.TemperatureSensor
+
+func NewLinuxSensors(info accessory.Info) *LinuxSensors {
+	acc := LinuxSensors{}
+	acc.Accessory = accessory.New(info, accessory.TypeSensor)
+
+	acc.Chips = make(map[string]*SensorChipValues)
+	// acc.StatelessSwitch = service.NewStatelessProgrammableSwitch()
+	// acc.AddService(acc.StatelessSwitch.Service)
+
+	return &acc
+}
