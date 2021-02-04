8940e9af019da5eadb571e138640f7f405327a48
Scot Bontrager <cloudkucooland@gmail.com>
2021-02-03 19:35:16 -0500

    work on konnected

--- config/config.go
+++ config/config.go
@@ -4,21 +4,22 @@ import (
 	"github.com/brutella/hc"
 )
 
-// Config is the primary daemon configuration...
+// Config is the global daemon configuration...
 type Config struct {
-	ConfigDir        string    // passed in from CLI
-	ConfigFile       string    // server.json
-	HTTPAddress      string    // net.Dial address format, :port is good enough
-	Name             string    // what this bridge shows as
-	ID               string    // displayed serial number -- if you run multiple instances, make sure each has a distinct ID
-	HCConfig         hc.Config // base HomeControl configuration
-	Discover         bool      // run Kasa & Shelly auto-discovery (does not work properly yet, do not enable)
-	KasaPullRate     uint8     // (seconds) how frequently to pull Kasa devices -- 0 to disable
-	KasaBroadcasts   uint8     // number of UDP broadcast packets to send - 1 is usually enough -- (unset/0/1 sends 1 packet)
-	KasaTimeout      uint8     // how long to wait for direct (TCP) pulls
-	ShellyPullRate   uint8     // 0 to disable pulling
-	ShellyTimeout    uint8     // how long to wait for direct pulls
-	KonnectedTimeout uint8     /// how long to wait for direct pulls
+	ConfigDir         string    // passed in from CLI
+	ConfigFile        string    // server.json
+	HTTPAddress       string    // net.Dial address format, :port is good enough
+	Name              string    // what this bridge shows as
+	ID                string    // displayed serial number -- if you run multiple instances, make sure each has a distinct ID
+	HCConfig          hc.Config // base HomeControl configuration
+	Discover          bool      // run Kasa, Konnected, & Shelly auto-discovery (does not work properly yet, do not enable)
+	KasaPullRate      uint8     // (seconds) how frequently to pull Kasa devices -- 0 to disable
+	KasaBroadcasts    uint8     // number of UDP broadcast packets to send - 1 is usually enough -- (unset/0/1 sends 1 packet)
+	KasaTimeout       uint8     // how long to wait for direct (TCP) pulls
+	ShellyPullRate    uint8     // 0 to disable pulling
+	ShellyTimeout     uint8     // how long to wait for direct pulls
+	KonnectedTimeout  uint8     /// how long to wait for direct pulls
+	KonnectedPullRate uint8     /// how frequently to pull , 0 to disable
 }
 
 var runningConfig *Config
--- devices/konnected.go
+++ devices/konnected.go
@@ -2,14 +2,14 @@ package devices
 
 import (
 	"github.com/brutella/hc/accessory"
-	// "github.com/brutella/hc/characteristic"
+	"github.com/brutella/hc/characteristic"
+	"github.com/brutella/hc/log"
 	"github.com/brutella/hc/service"
 )
 
 type Konnected struct {
 	*accessory.Accessory
 
-	Mac            string
 	SecuritySystem *KonnectedSvc
 }
 
@@ -19,21 +19,35 @@ func NewKonnected(info accessory.Info) *Konnected {
 
 	acc.SecuritySystem = NewKonnectedSvc()
 	acc.AddService(acc.SecuritySystem.Service)
+	acc.SecuritySystem.SecuritySystemTargetState.OnValueRemoteUpdate(func(newval int) {
+		// do the work to adjust the state
+		log.Info.Printf("HC requested system state change to %d", newval)
+		acc.SecuritySystem.SecuritySystemCurrentState.SetValue(newval)
+	})
+
+	alarmType := characteristic.NewSecuritySystemAlarmType()
+	alarmType.SetValue(1)
+	acc.SecuritySystem.AddCharacteristic(alarmType.Characteristic)
+
 	return &acc
 }
 
 type KonnectedSvc struct {
 	*service.Service
 
-	// On         *characteristic.On
+	SecuritySystemCurrentState *characteristic.SecuritySystemCurrentState
+	SecuritySystemTargetState  *characteristic.SecuritySystemTargetState
 }
 
 func NewKonnectedSvc() *KonnectedSvc {
 	svc := KonnectedSvc{}
 	svc.Service = service.New(service.TypeSecuritySystem)
 
-	// svc.On = characteristic.NewOn()
-	// svc.AddCharacteristic(svc.On.Characteristic)
+	characteristic.NewSecuritySystemCurrentState()
+	svc.AddCharacteristic(svc.SecuritySystemCurrentState.Characteristic)
+
+	svc.SecuritySystemTargetState = characteristic.NewSecuritySystemTargetState()
+	svc.AddCharacteristic(svc.SecuritySystemTargetState.Characteristic)
 
 	return &svc
 }
