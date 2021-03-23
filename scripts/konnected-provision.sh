511f1b8d60c5663815733e54b449c60286706711
Scot Bontrager <cloudkucooland@gmail.com>
2021-03-23 17:24:18 -0400

    add

---
+++ scripts/konnected-provision.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# push a new config to the konnected.io device (not pro)
+#
+#{"actuators":[ {"pin":8, "trigger": 1} ]}
+#
+curl -X PUT -H "Content-Type: application/json" -d '{"endpoint_type":"rest","endpoint":"http://192.168.1.253:8444/konnected/", "token":"not yet", "sensors":[{"pin":1},{"pin":2},{"pin":5},{"pin":6},{"pin":7},{"pin":9}]}' http://192.168.1.226:15301/settings
