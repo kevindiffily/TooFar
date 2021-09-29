ca0dc8e773d8bd3a644b3b97719e4f64ef7e879f
Scot Bontrager <scot@indievisible.org>
2021-09-29 14:30:57 -0500

    update

--- scripts/konnected-provision.sh
+++ scripts/konnected-provision.sh
@@ -4,4 +4,4 @@
 #
 #{"actuators":[ {"pin":8, "trigger": 1} ]}
 #
-curl -X PUT -H "Content-Type: application/json" -d '{"endpoint_type":"rest", "endpoint":"http://192.168.12.163:8444/konnected", "token":"notyet", "sensors":[{"pin":1},{"pin":2},{"pin":5},{"pin":6},{"pin":7},{"pin":9}], "actuators":[{"pin":8, "trigger": 1}]}' http://192.168.12.168:15301/settings
+curl -X PUT -H "Content-Type: application/json" -d '{"endpoint_type":"rest", "endpoint":"http://192.168.12.166:8444/konnected", "token":"notyet", "sensors":[{"pin":1},{"pin":2},{"pin":5},{"pin":6},{"pin":7},{"pin":9}], "actuators":[{"pin":8, "trigger": 1}]}' http://192.168.12.169:15301/settings
